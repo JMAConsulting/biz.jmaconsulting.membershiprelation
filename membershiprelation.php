c7455bd9f3f5f58cc8217943e115ca26b12f7e99
Monish Deb <deb.monish@gmail.com>
2019-10-13 23:28:36 +0530

    add WP user

--- membershiprelation.php
+++ membershiprelation.php
@@ -333,7 +333,7 @@
         createRelationshipMember($child1, $parent1, $childRel);
 
         // create wp user
-        //CRM_Volunteerreference_Utils::createWpUser($parent1);
+        createWpUser($parent1, TRUE);
 
         foreach ($contact as $person => $con) {
           if (in_array($person, ['child2', 'child3', 'child4']) && !empty($contact[$person][0])) {
@@ -389,6 +389,54 @@
       }
       */
     }
+  }
+
+  function createWPUser($contactID, $sendResetLink = FALSE) {
+    $contact = civicrm_api3('Contact', 'getsingle', ['id' => $contactID]);
+    $ufID = CRM_Core_DAO::getFieldValue('CRM_Core_BAO_UFMatch', $contactID, 'uf_id', 'contact_id');
+    $ufID = $ufID ?:
+      (!empty($contact['email'])) ?
+        CRM_Core_DAO::singleValueQuery("SELECT MAX(uf_id) FROM civicrm_uf_match WHERE uf_name =  '" . $contact['email'] . "' ") :
+          NULL;
+
+    if (!$ufID) {
+      $cmsName = strtolower($contact['first_name'] . '.' . $contact['last_name'] . '.' . $contact['id']);
+      $params = [
+        'contactID' => $contact['id'],
+        'cms_pass' => 'changeme',
+        'cms_name' => $cmsName,
+        'email' => $contact['email'],
+      ];
+      $ufID = CRM_Core_BAO_CMSUser::create($params, 'email');
+    }
+    $u = new WP_User($ufID);
+    if ($sendResetLink) {
+      sendResetLink($u);
+    }
+  }
+
+  function sendResetLink($user) {
+    $firstname = $user->first_name;
+    $email = $user->user_email;
+    $adt_rp_key = get_password_reset_key( $user );
+    $user_login = $user->user_login;
+    $rp_link = '<a href="' . wp_login_url()."/resetpass/?key=$adt_rp_key&login=" . rawurlencode($user_login) . '">' . wp_login_url()."/resetpass/?key=$adt_rp_key&login=" . rawurlencode($user_login) . '</a>';
+
+    $message = "Hi ".$firstname.",<br>";
+    $message .= "An account has been created on ".get_bloginfo( 'name' )." for email address ".$email."<br>";
+    $message .= "Click here to set the password for your account: <br>";
+    $message .= $rp_link.'<br>';
+
+    //deze functie moet je zelf nog toevoegen.
+    $subject = __("Your account on ".get_bloginfo( 'name'));
+    $headers = array();
+
+    add_filter( 'wp_mail_content_type', function( $content_type ) {return 'text/html';});
+    $headers[] = 'From: Your company name <cagis@uwo.ca>'."\r\n";
+    wp_mail( $email, $subject, $message, $headers);
+
+    // Reset content-type to avoid conflicts -- http://core.trac.wordpress.org/ticket/23578
+    remove_filter( 'wp_mail_content_type', 'set_html_content_type' );
   }
 
   function createRelationshipMember($cida, $cidb, $type) {
